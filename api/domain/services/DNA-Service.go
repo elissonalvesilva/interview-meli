f638f55df1c309fc58b4504cbeb0e0d68ba4672e
Elisson Alves Silva <elissonalvessilva@192.168.1.7>
2021-06-24 18:17:48 -0400

    feat: add CreateDNA in service

--- api/domain/services/DNA-Service.go
+++ api/domain/services/DNA-Service.go
@@ -1,8 +1,11 @@
 package services
 
 import (
+	"errors"
+	"github.com/elissonalvesilva/interview-meli/api/domain/entity"
 	"github.com/elissonalvesilva/interview-meli/api/domain/protocols"
 	"github.com/elissonalvesilva/interview-meli/api/domain/repository"
+	"github.com/elissonalvesilva/interview-meli/api/shared/constants"
 )
 
 type DNAService struct {
@@ -23,4 +26,27 @@ func (s *DNAService) GetStatsSimianHuman() (protocols.StatsResponse, error) {
 	}
 
 	return stats, nil
+}
+
+func (s *DNAService) CreateDNA(dna []string) (entity.DNASequence, error) {
+	createdDNA, errCreateDNA := entity.NewDNA(dna)
+	if errCreateDNA != nil {
+		return entity.DNASequence{}, errCreateDNA
+	}
+
+	alreadyInDB, err := s.repo.CheckIfDNAExists(createdDNA.DNA)
+	if err != nil {
+		return entity.DNASequence{}, err
+	}
+
+	if alreadyInDB {
+		return entity.DNASequence{}, errors.New(constants.DNAExists)
+	}
+
+	errToAddDNA := s.repo.AddDNA(createdDNA.DNA)
+	if errToAddDNA != nil {
+		return entity.DNASequence{}, errToAddDNA
+	}
+
+	return *createdDNA, nil
 }
