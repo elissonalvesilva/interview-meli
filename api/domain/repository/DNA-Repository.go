77761fab4467a5b558fb64e6657b0afcbc946a87
Elisson Alves Silva <elissonalvessilva@192.168.1.7>
2021-06-24 18:17:24 -0400

    feat: created new function in repository

--- api/domain/repository/DNA-Repository.go
+++ api/domain/repository/DNA-Repository.go
@@ -1,7 +1,11 @@
 package repository
 
-import "github.com/elissonalvesilva/interview-meli/api/domain/protocols"
+import (
+	"github.com/elissonalvesilva/interview-meli/api/domain/protocols"
+)
 
 type DNARepository interface {
+	CheckIfDNAExists([]string) (bool, error)
+	AddDNA([]string) error
 	StatsHumanSimian() (protocols.StatsResponse, error)
 }
