e2e26a88364bef3274a5ece38512f9613dcb3378
Elisson Alves Silva <elissonalvessilva@192.168.1.7>
2021-06-26 22:41:33 -0400

    feat: created analyzer controller

--- analyzer/presenters/controllers/Analyzer-Controller.go
+++ analyzer/presenters/controllers/Analyzer-Controller.go
@@ -1,1 +1,18 @@
 package controllers
+
+import "github.com/elissonalvesilva/interview-meli/analyzer/domain/services"
+
+type AnalyzerController struct {
+	service services.Analyzer
+}
+
+func NewAnalyzerController(service services.Analyzer) *AnalyzerController {
+	return &AnalyzerController{
+		service: service,
+	}
+}
+
+func (s *AnalyzerController) AnalyzeDNA(dna []string) (string, error) {
+	typeDNA, err := s.service.DNAAnalyzer(dna)
+	return typeDNA, err
+}
