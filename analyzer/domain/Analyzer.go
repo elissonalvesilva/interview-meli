a628de1c8f02ba72016f3cd36b41bb70100b75f9
Elisson Alves Silva <elissonalvessilva@192.168.1.7>
2021-06-26 20:53:59 -0400

    feat: created dna analyzer

---
+++ analyzer/domain/Analyzer.go
@@ -0,0 +1,112 @@
+package domain
+
+import (
+	constants "github.com/elissonalvesilva/interview-meli/analyzer/shared"
+	"strings"
+)
+
+type Analyzer struct {}
+
+const sequencesNeeded = 1
+const consecutiveLettersNeeded = 4
+var MatrixLength int
+var validLetters = map[string]int{
+	"A": 1,
+	"C": 2,
+	"G": 3,
+	"T": 4,
+}
+
+func NewAnalyzer() *Analyzer {
+	return &Analyzer{}
+}
+
+func IsValidMatrix(dna []string) bool {
+	return len(dna) == len(dna[0])
+}
+
+func (a *Analyzer) DNAAnalyzer(dna []string) (string, error) {
+	var matrix, err = GetMatrixFromDNAList(dna)
+
+	count := 0
+
+	if err != nil {
+		return constants.EmptyDNA, err
+	}
+
+	MatrixLength = len(dna)
+	dnaMatrixLength := len(dna)
+
+	for row := 0; row < dnaMatrixLength; row++ {
+		for col := 0; col < dnaMatrixLength; col++ {
+			currentLetter := matrix[row][col]
+
+			if SearchForSimianValidRepetitions(matrix, row, col, row, col, 0, currentLetter) {
+				count += 1
+				if count >= sequencesNeeded {
+					return constants.DNATypeSimian, nil
+				}
+			}
+		}
+	}
+
+	return constants.DNATypeHuman, nil
+}
+
+func SearchForSimianValidRepetitions(matrix [][]string, lastRow int, lastCol int, currentRow int, currentCol int, countLettersFound int, nextLetter string) bool {
+	if countLettersFound == consecutiveLettersNeeded {
+		return true
+	}
+
+	if !isValidCoordinate(currentRow, currentCol) || !(matrix[currentRow][currentCol] == nextLetter) {
+		return false
+	}
+
+	var result = false
+
+	if countLettersFound > 0 {
+		sumRow := currentRow - lastRow
+		sumCol := currentCol - lastCol
+		result = SearchForSimianValidRepetitions(matrix, currentRow, currentCol, currentRow + sumRow, currentCol + sumCol, countLettersFound + 1, nextLetter)
+	}else {
+		result = SearchForSimianValidRepetitions(matrix, lastRow, lastCol, currentRow -1, currentCol + 1, 1, nextLetter) ||
+			SearchForSimianValidRepetitions(matrix, lastRow, lastCol, currentRow, currentCol + 1, 1, nextLetter) ||
+			SearchForSimianValidRepetitions(matrix, lastRow, lastCol, currentRow + 1, currentCol +1, 1, nextLetter) ||
+			SearchForSimianValidRepetitions(matrix, lastRow, lastCol, currentRow + 1, currentCol, 1, nextLetter)
+	}
+
+	return result
+}
+
+func GetMatrixFromDNAList(dna []string) ([][]string, error){
+	if dna == nil {
+		return nil, ErrDNAEmpty
+	}
+
+	if !IsValidMatrix(dna) {
+		return nil, ErrInvalidMatrix
+	}
+
+	matrixLength := len(dna)
+	var matrixResult [][]string
+
+	for row := 0; row < matrixLength; row++ {
+
+		rowArray := strings.Split(dna[row], "")
+
+		for col := 0; col < len(rowArray); col++ {
+			currentCol := rowArray[col]
+			if _, exists := validLetters[currentCol]; !exists {
+				return nil, ErrInvalidCharMatrix
+			}
+		}
+		matrixResult = append(matrixResult, rowArray)
+
+	}
+
+	return matrixResult, nil
+}
+
+func isValidCoordinate(actualRow int, actualCol int) bool {
+	return !(actualRow <0 || actualRow >= MatrixLength || actualCol < 0 || actualCol >= MatrixLength)
+}
---
+++ analyzer/domain/Errors-Analyzer.go
@@ -0,0 +1,15 @@
+package domain
+
+import "errors"
+
+const (
+	errDNAEmptyMessage = "DNA must be pass to function"
+	errInvalidMatrix = "Matrix must be square. ex: 5x5 or 4x4"
+	errInvalidCharDNA = "Invalid char in the matrix, only permitted: G, T, A, C."
+)
+
+var (
+	ErrDNAEmpty = errors.New(errDNAEmptyMessage)
+	ErrInvalidMatrix = errors.New(errInvalidMatrix)
+	ErrInvalidCharMatrix = errors.New(errInvalidCharDNA)
+)
