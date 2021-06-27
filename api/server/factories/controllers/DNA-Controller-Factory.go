56975600ce4be985b7c75d8604543b42a0472884
Elisson Alves Silva <elissonalvessilva@192.168.1.7>
2021-06-26 23:33:09 -0400

    feat: created factories

--- api/server/factories/controllers/DNA-Controller-Factory.go
+++ api/server/factories/controllers/DNA-Controller-Factory.go
@@ -1,1 +1,13 @@
 package controllers
+
+import (
+	"github.com/elissonalvesilva/interview-meli/api/infra/db/mongodb/helper"
+	"github.com/elissonalvesilva/interview-meli/api/presenters/controllers"
+	"github.com/elissonalvesilva/interview-meli/api/server/factories/services"
+)
+
+func MakeDNAController(db helper.MongoDB) *controllers.DNAController {
+	service := services.MakeDNAService(db)
+	controller := controllers.NewDNAController(*service)
+	return controller
+}
--- api/server/factories/services/DNA-Service-Factory.go
+++ api/server/factories/services/DNA-Service-Factory.go
@@ -1,1 +1,14 @@
 package services
+
+import (
+	"github.com/elissonalvesilva/interview-meli/api/domain/services"
+	"github.com/elissonalvesilva/interview-meli/api/infra/db/mongodb"
+	"github.com/elissonalvesilva/interview-meli/api/infra/db/mongodb/helper"
+)
+
+func MakeDNAService(db helper.MongoDB) *services.DNAService {
+	repository := mongodb.NewDNADatabase(db)
+	service := services.NewDNAService(repository)
+
+	return service
+}
