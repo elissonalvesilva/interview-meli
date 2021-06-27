a83e3d625dae4d34db26df0e00055319f2d2b586
Elisson Alves Silva <elissonalvessilva@192.168.1.7>
2021-06-26 22:41:49 -0400

    test: test analyzer controller

--- analyzer/presenters/controllers/Analyzer-Controller_test.go
+++ analyzer/presenters/controllers/Analyzer-Controller_test.go
@@ -1,1 +1,33 @@
 package controllers
+
+import (
+	"github.com/elissonalvesilva/interview-meli/analyzer/domain/services"
+	"github.com/elissonalvesilva/interview-meli/analyzer/tests/mock"
+	"github.com/elissonalvesilva/interview-meli/api/shared/constants"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAnalyzerController(t *testing.T) {
+	t.Run("Should return empty type DNA and a error", func(t *testing.T) {
+		t.Parallel()
+
+		service := services.NewAnalyzer()
+		sut := NewAnalyzerController(*service)
+
+		typeDNA, err := sut.AnalyzeDNA(nil)
+		assert.Error(t, err, services.ErrDNAEmpty)
+		assert.Equal(t, "", typeDNA)
+	})
+
+	t.Run("Should return a type dna and error equals to nil", func(t *testing.T) {
+		t.Parallel()
+
+		service := services.NewAnalyzer()
+		sut := NewAnalyzerController(*service)
+
+		typeDNA, err := sut.AnalyzeDNA(mock.DNAValid)
+		assert.Nil(t, err)
+		assert.Equal(t, constants.DNATypeSimian, typeDNA)
+	})
+}
