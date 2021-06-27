8296a57fb01eb72dab5eb9468a900e27aeec5b12
Elisson Alves Silva <elissonalvessilva@192.168.1.7>
2021-06-27 02:00:42 -0400

    feat: created DNA analyze service

--- api/domain/services/DNA-Analyze-Service.go
+++ api/domain/services/DNA-Analyze-Service.go
@@ -1,1 +1,22 @@
 package services
+
+import "github.com/elissonalvesilva/interview-meli/api/domain/repository"
+
+type DNAAnalyzeService struct {
+	grpcClientAnalyze repository.AnalyzeGRPC
+}
+
+func NewDNAAnalyzeService(grpcClientAnalyze repository.AnalyzeGRPC) *DNAAnalyzeService {
+	return &DNAAnalyzeService{
+		grpcClientAnalyze: grpcClientAnalyze,
+	}
+}
+
+func (client *DNAAnalyzeService) Analyze(dna []string) (string, error) {
+	analyzedDNA, err := client.grpcClientAnalyze.AnalyzeDNA(dna)
+	if err != nil {
+		return analyzedDNA, err
+	}
+
+	return analyzedDNA, nil
+}
