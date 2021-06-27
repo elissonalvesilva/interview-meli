d35fa0da597f3665de2928a4595584c6e4d200f5
Elisson Alves Silva <elissonalvessilva@192.168.1.7>
2021-06-27 02:06:20 -0400

    feat: integrate client grpc server analyzer

--- api/server/factories/services/DNA-Service-Factory.go
+++ api/server/factories/services/DNA-Service-Factory.go
@@ -4,11 +4,14 @@ import (
 	"github.com/elissonalvesilva/interview-meli/api/domain/services"
 	"github.com/elissonalvesilva/interview-meli/api/infra/db/mongodb"
 	"github.com/elissonalvesilva/interview-meli/api/infra/db/mongodb/helper"
+	analyzer_grpc_client "github.com/elissonalvesilva/interview-meli/api/infra/grpc/analyzer-grpc-client"
 )
 
 func MakeDNAService(db helper.MongoDB) *services.DNAService {
 	repository := mongodb.NewDNADatabase(db)
-	service := services.NewDNAService(repository)
+	serviceGRPC := analyzer_grpc_client.NewAnalyzerService()
+	analyzerService := services.NewDNAAnalyzeService(serviceGRPC)
+	service := services.NewDNAService(repository, *analyzerService)
 
 	return service
 }
